80587a19ace44163e23f16f7d1c5088737388a9b
Haonan Pei <>
2023-12-15 12:09:45 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "instance_id" {
   value       = aws_instance.aws-terraform-node.id
 }
 
+output "instance_arn" {
+  description = "ARN of the EC2 instance"
+  value       = aws_instance.aws-terraform-node.arn
+}
+
 output "instance_private_ip" {
   description = "Private IP address of the EC2 instance"
   value       = aws_instance.aws-terraform-node.private_ip
