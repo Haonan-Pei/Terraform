e112bc0433b40bbaa9de5dea554bdcdf4d54d010
Haonan Pei <>
2023-12-15 15:50:00 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,22 +13,8 @@ variable "instance_type" {
   description = "AWS EC2 Instance Type"
 }
 
-variable "root_volume_type" {
-  description = "AWS EC2 Root Volume Type"
-  default = "gp3"
-}
-
 variable "root_volume_size" {
   description = "AWS EC2 Root Volume Size"
-}
-
-variable "ebs_volume_type" {
-  description = "AWS EC2 Data Volume Type"
-  default = "gp3"
-}
-
-variable "ebs_volume_size" {
-  description = "AWS EC2 Data Volume Size"
 }
 
 variable "subnet_id" {
