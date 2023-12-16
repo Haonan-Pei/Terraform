eb3ef8722c683b32bc5aeee30c729b04a8d89913
Haonan Pei <>
2023-12-16 19:28:46 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ data "aws_ami" "amazon_linux_2" {
 
 
 resource "aws_instance" "aws-terraform-node-without-module" {
-  count = length([var.name])
+  count = length(var.name)
 
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
