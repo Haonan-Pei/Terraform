90d71835725a66eacf6ff233fbc16f02690f669d
Haonan Pei <>
2023-12-16 16:18:13 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,19 +7,31 @@ terraform {
   }
 }
 
+variable "ec2instance_ebs_volume_size" {}
+variable "ec2instance_instance_number" {}
+variable "ec2instance_instance_type" {}
+variable "ec2instance_key_name" {}
+variable "ec2instance_region" {}
+variable "ec2instance_role_arn" {}
+variable "ec2instance_root_volume_size" {}
+variable "ec2instance_session_name" {}
+variable "ec2instance_subnet_id" {}
+variable "ec2instance_tags" {}
+variable "ec2instance_vpc_security_group_ids" {}
+
 module "ec2instance" {
   source  = "app.terraform.io/Haonan-Pei/ec2instance/aws"
   version = "1.0.7"
 
-  region = module.ec2instance.region
-  instance_number = module.ec2instance.instance_number
-  vpc_security_group_ids = module.ec2instance.vpc_security_group_ids
-  role_arn     = module.ec2instance.role_arn
-  session_name = module.ec2instance.session_name
-  instance_type = module.ec2instance.instance_type
-  subnet_id       = module.ec2instance.subnet_id
-  root_volume_size = module.ec2instance.root_volume_size
-  ebs_volume_size  = module.ec2instance.ebs_volume_size
-  key_name  = module.ec2instance.key_name
-  tags = module.ec2instance.tags
+  ebs_volume_size = "${var.ec2instance_ebs_volume_size}"
+  instance_number = "${var.ec2instance_instance_number}"
+  instance_type = "${var.ec2instance_instance_type}"
+  key_name = "${var.ec2instance_key_name}"
+  region = "${var.ec2instance_region}"
+  role_arn = "${var.ec2instance_role_arn}"
+  root_volume_size = "${var.ec2instance_root_volume_size}"
+  session_name = "${var.ec2instance_session_name}"
+  subnet_id = "${var.ec2instance_subnet_id}"
+  tags = "${var.ec2instance_tags}"
+  vpc_security_group_ids = "${var.ec2instance_vpc_security_group_ids}"
 }
