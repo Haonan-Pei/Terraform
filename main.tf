3840c20373b58aab3cfb675f99822447ca3f9868
Haonan Pei <>
2023-12-16 16:10:30 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 module "ec2instance" {
   source  = "app.terraform.io/Haonan-Pei/ec2instance/aws"
-  version = "1.0.6"
+  version = "1.0.7"
 
   region = module.region
   instance_number = module.instance_number
