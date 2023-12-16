6061e18a3b708089fd924c3ee09b4230651f53eb
Haonan Pei <>
2023-12-16 14:33:43 +0800

    Update main.tf

--- aws-ec2-instance/main.tf
+++ aws-ec2-instance/main.tf
@@ -6,62 +6,7 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-    region     = var.region
-    assume_role {
-      role_arn     = var.role_arn
-      session_name = var.session_name
-    }
-}
-
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  owners = ["amazon"]
-}
-
-
-resource "aws_instance" "aws-terraform-node" {
-  count = var.instance_number
-
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = var.instance_type
-
-  subnet_id       = var.subnet_id
-  security_groups = ["var.vpc_security_groups_ids"]
-  root_block_device {
-    encrypted             = "true"
-    volume_type           = "gp3"
-    volume_size           = var.root_volume_size
-    delete_on_termination = "true"
-  }
-
-  ebs_block_device {
-    encrypted             = "true"
-    device_name           = "/dev/sdb"
-    volume_type           = "gp3"
-    volume_size           = var.ebs_volume_size
-    delete_on_termination = "true"
-  }
-
-  user_data = var.user_data
-  key_name  = var.key_name
-
-  tags = var.tags
+module "ec2_instance" {
+  source  = "app.terraform.io/Haonan-Pei/ec2-instance/aws"
+  version = "1.0.0"
 }
