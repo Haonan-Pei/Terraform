658655059db598ecd9aa319de03d629e30cbf176
Haonan Pei <>
2023-12-16 17:29:21 +0800

    Update

--- main.tf
+++ main.tf
@@ -7,19 +7,60 @@ terraform {
   }
 }
 
-module "ec2instance" {
-  source  = "app.terraform.io/Haonan-Pei/ec2instance/aws"
-  version = "1.0.13"
+provider "aws" {
+    region     = var.region
+    assume_role {
+      role_arn     = var.role_arn
+      session_name = var.session_name
+    }
+}
 
-  region = var.region
-  instance_number = var.instance_number
-  vpc_security_group_ids = [var.vpc_security_group_ids]
-  role_arn     = var.role_arn
-  session_name = var.session_name
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  owners = ["amazon"]
+}
+
+
+resource "aws_instance" "aws-terraform-node" {
+  count = var.instance_number
+
+  ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
+
   subnet_id       = var.subnet_id
-  root_volume_size = var.root_volume_size
-  ebs_volume_size  = var.ebs_volume_size
+  vpc_security_group_ids = var.vpc_security_group_ids
+  root_block_device {
+    encrypted             = "true"
+    volume_type           = "gp3"
+    volume_size           = var.root_volume_size
+    delete_on_termination = "true"
+  }
+
+  ebs_block_device {
+    encrypted             = "true"
+    device_name           = "/dev/sdb"
+    volume_type           = "gp3"
+    volume_size           = var.ebs_volume_size
+    delete_on_termination = "true"
+  }
+
   key_name  = var.key_name
+
   tags = var.tags
 }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,23 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = module.ec2instance.instance_id
+  value       = try(
+    aws_instance.aws-terraform-node[0].id,
+    null,
+  )
 }
 
 output "instance_arn" {
   description = "ARN of the EC2 instance"
-  value       = module.ec2instance.instance_arn
+  value       = try(
+    aws_instance.aws-terraform-node[0].id,
+    null,
+  )
 }
 
 output "instance_private_ip" {
   description = "Private IP address of the EC2 instance"
-  value       = module.ec2instance.instance_private_ip
+  value       = try(
+    aws_instance.aws-terraform-node[0].private_ip,
+    null,
+  )
 }
