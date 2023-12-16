9fe708779984a6792759045d59f70456d52c0b9a
Haonan Pei <>
2023-12-16 16:33:37 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "ec2instance" {
 
   region = var.region
   instance_number = var.instance_number
-  vpc_security_group_ids = ["var.vpc_security_group_ids"]
+  vpc_security_group_ids = [var.vpc_security_group_ids]
   role_arn     = var.role_arn
   session_name = var.session_name
   instance_type = var.instance_type
