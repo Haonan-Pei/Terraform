eb1587fdd2fe4a27f9a02dffb268822d147021bb
Haonan Pei <>
2023-12-16 16:12:44 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,15 +11,15 @@ module "ec2instance" {
   source  = "app.terraform.io/Haonan-Pei/ec2instance/aws"
   version = "1.0.7"
 
-  region = module.region
-  instance_number = module.instance_number
-  vpc_security_group_ids = module.vpc_security_group_ids
-  role_arn     = module.role_arn
-  session_name = module.session_name
-  instance_type = module.instance_type
-  subnet_id       = module.subnet_id
-  root_volume_size = module.root_volume_size
-  ebs_volume_size  = module.ebs_volume_size
-  key_name  = module.key_name
-  tags = module.tags
+  region = module.ec2instance.region
+  instance_number = module.ec2instance.instance_number
+  vpc_security_group_ids = module.ec2instance.vpc_security_group_ids
+  role_arn     = module.ec2instance.role_arn
+  session_name = module.ec2instance.session_name
+  instance_type = module.ec2instance.instance_type
+  subnet_id       = module.ec2instance.subnet_id
+  root_volume_size = module.ec2instance.root_volume_size
+  ebs_volume_size  = module.ec2instance.ebs_volume_size
+  key_name  = module.ec2instance.key_name
+  tags = module.ec2instance.tags
 }
