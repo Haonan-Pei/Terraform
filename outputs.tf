713e6a696e8cba6896022d4e6ee5ac0b9792b9fe
Haonan Pei <>
2023-12-16 17:47:02 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,23 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.aws-terraform-node.id
+  value       = try(
+    aws_instance.aws-terraform-node[0].id,
+    null,
+  )
 }
 
 output "instance_arn" {
   description = "ARN of the EC2 instance"
-  value       = aws_instance.aws-terraform-node.arn
+  value       = try(
+    aws_instance.aws-terraform-node[0].arn,
+    null,
+  )
 }
 
 output "instance_private_ip" {
   description = "Private IP address of the EC2 instance"
-  value       = aws_instance.aws-terraform-node.private_ip
+  value       = try(
+    aws_instance.aws-terraform-node[0].private_ip,
+    null,
+  )
 }
