756df58dbdde0aca13fac334e0144f7570ec92c9
Haonan Pei <>
2023-12-16 14:56:30 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "region" {
+  description = "AWS Region"
+}
+
+variable "role_arn" {
+  description = "AWS IAM Role ARN"
+}
+
+variable "session_name" {
+  description = "AWS IAM Role Session Name"
+}
+
+variable "instance_number" {
+  description = "Number"
+}
+
+variable "instance_type" {
+  description = "AWS EC2 Instance Type"
+}
+
+variable "key_name" {
+  description = "AWS Key Pair"
+}
+
+variable "root_volume_size" {
+  description = "AWS EC2 Root Volume Size"
+}
+
+variable "ebs_volume_size" {
+  description = "AWS EC2 Data Volume Size"
+}
+
+variable "subnet_id" {
+  description = "AWS Subnet ID"
+}
+
+variable "vpc_security_groups_ids" {
+  description = "AWS Security Group ID"
+}
+
+variable "user_data" {
+  description = "AWS EC2 User Data"
+}
+
+variable "tags" {
+  description = "AWS Tags"
+  type = map(string)
+}
