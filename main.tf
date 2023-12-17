6758922c5b95055395189627370174ece6789a2b
Haonan Pei <>
2023-12-17 17:18:49 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_instance" "aws-terraform-node-without-module" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
 
-  subnet_id       = element(data.aws_subnets.aws-private-subnet.id, count.index)
+  subnet_id       = element(data.aws_subnet.aws-private-subnet.id, count.index)
   vpc_security_group_ids = [var.vpc_security_group_ids]
   root_block_device {
     encrypted             = "true"
