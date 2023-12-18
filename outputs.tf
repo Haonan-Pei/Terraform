7b6682c297f901d3e5c6454cad55e6901cf0fe07
Haonan Pei <>
2023-12-18 15:38:35 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,34 @@
 output "db_instance_name" {
   description = "AWS RDS Instance Name"
-  value       = aws_db_instance.aws-terraform-rds.db_instance_name
+  value       = aws_db_instance.aws-terraform-rds.identifier
 }
 
 output "db_instance_arn" {
   description = "AWS RDS Instance ARN"
-  value       = aws_db_instance.aws-terraform-rds.db_instance_arn
+  value       = aws_db_instance.aws-terraform-rds.arn
 }
 
 output "db_instance_status" {
   description = "AWS RDS Instance Status"
-  value       = aws_db_instance.aws-terraform-rds.db_instance_status
+  value       = aws_db_instance.aws-terraform-rds.status
 }
 
 output "db_instance_engine" {
   description = "AWS RDS Instance Engine"
-  value       = aws_db_instance.aws-terraform-rds.db_instance_engine
+  value       = aws_db_instance.aws-terraform-rds.engine
 }
 
-output "db_instance_engine_version_actual" {
+output "db_instance_engine_version" {
   description = "AWS RDS Instance Engine Version"
-  value       = aws_db_instance.aws-terraform-rds.db_instance_engine_version_actual
+  value       = aws_db_instance.aws-terraform-rds.engine_version
 }
 
 output "db_instance_port" {
   description = "AWS RDS Instance Port"
-  value       = aws_db_instance.aws-terraform-rds.db_instance_port
+  value       = aws_db_instance.aws-terraform-rds.port
 }
 
 output "db_instance_endpoint" {
   description = "AWS RDS Instance Endpoint"
-  value       = aws_db_instance.aws-terraform-rds.db_instance_endpoint
+  value       = aws_db_instance.aws-terraform-rds.endpoint
 }
