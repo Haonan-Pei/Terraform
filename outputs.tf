674bcd9c1e74be2b4bc428a107788e041c0e8810
Haonan Pei <>
2023-12-19 14:48:03 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,61 +1,61 @@
 ##### EC2 #####
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = module.ec2_rds.aws-terraform-node[*].id
+  value       = module.ec2_rds.id
 }
 
 output "instance_arn" {
   description = "ARN of the EC2 instance"
-  value       = module.ec2_rds.aws-terraform-node[*].arn
+  value       = module.ec2_rds.arn
 }
 
 output "instance_private_ip" {
   description = "Private IP address of the EC2 instance"
-  value       = module.ec2_rds.aws-terraform-node[*].private_ip
+  value       = module.ec2_rds.private_ip
 }
 
 ##### RDS #####
 output "db_instance_name" {
   description = "AWS RDS Instance Name"
-  value       = module.ec2_rds.aws-terraform-rds.identifier
+  value       = module.ec2_rds.identifier
 }
 
 output "db_instance_arn" {
   description = "AWS RDS Instance ARN"
-  value       = module.aws_db_instance.aws-terraform-rds.arn
+  value       = module.ec2_rds.arn
 }
 
 output "db_instance_status" {
   description = "AWS RDS Instance Status"
-  value       = aws_db_instance.aws-terraform-rds.status
+  value       = module.ec2_rds.status
 }
 
 output "db_instance_engine" {
   description = "AWS RDS Instance Engine"
-  value       = aws_db_instance.aws-terraform-rds.engine
+  value       = module.ec2_rds.engine
 }
 
 output "db_instance_engine_version" {
   description = "AWS RDS Instance Engine Version"
-  value       = aws_db_instance.aws-terraform-rds.engine_version
+  value       = module.ec2_rds.engine_version
 }
 
 output "db_instance_multi_az" {
   description = "AWS RDS Multi AZ Status"
-  value = aws_db_instance.aws-terraform-rds.multi_az
+  value = module.ec2_rds.multi_az
 }
 
 output "db_instance_availability_zone" {
   description = "AWS RDS Multi AZ Status"
-  value = aws_db_instance.aws-terraform-rds.availability_zone
+  value = module.ec2_rds.availability_zone
 }
 
 output "db_instance_port" {
   description = "AWS RDS Instance Port"
-  value       = aws_db_instance.aws-terraform-rds.port
+  value       = module.ec2_rds.port
 }
 
 output "db_instance_endpoint" {
   description = "AWS RDS Instance Endpoint"
-  value       = aws_db_instance.aws-terraform-rds.endpoint
+  value       = module.ec2_rds.endpoint
 }
