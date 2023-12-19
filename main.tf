2c890f3e60c76d255b405fc7460d3dc801b39055
Haonan Pei <>
2023-12-19 14:12:12 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,125 +15,61 @@ provider "aws" {
     }
 }
 
-##### EC2 #####
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
+variable "allocated_storage" {}
+variable "db_subnet_group_name" {}
+variable "ebs_01_volume_size" {}
+variable "ebs_02_volume_size" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "identifier" {}
+variable "instance_class" {}
+variable "instance_name" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "kms_key_id" {}
+variable "monitoring_role_arn" {}
+variable "multi_az" {}
+variable "option_group_name" {}
+variable "parameter_group_name" {}
+variable "password" {}
+variable "region" {}
+variable "role_arn" {}
+variable "root_volume_size" {}
+variable "session_name" {}
+variable "subnet_id" {}
+variable "tags" {}
+variable "username" {}
+variable "vpc_id" {}
+variable "vpc_security_group_ids" {}
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
+module "ec2_rds" {
+  source  = "app.terraform.io/eBaoTech/ec2_rds/aws"
+  version = "1.0.0"
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  owners = ["amazon"]
-}
-
-data "aws_vpc" "aws-vpc" {
-  id = var.vpc_id
-}
-
-data "aws_availability_zones" "aws-az" {
-  filter {
-    name   = "region-name"
-    values = [var.region]
-  }
-}
-data "aws_subnets" "aws-private-subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-
-  tags = {
-    type = "private"
-  }
-}
-
-data "aws_subnet" "aws-private-subnet" {
-  count = length(data.aws_subnets.aws-private-subnets.ids)
-  id    = data.aws_subnets.aws-private-subnets.ids[count.index]
-}
-
-locals {
-  ids_sorted_by_az = values(zipmap(data.aws_subnet.aws-private-subnet.*.availability_zone, data.aws_subnet.aws-private-subnet.*.id))
-}
-
-resource "aws_instance" "aws-terraform-node" {
-  count = length(var.instance_name)
-  ami           = data.aws_ami.amazon_linux_2.id
+  allocated_storage = var.allocated_storage
+  db_subnet_group_name = var.db_subnet_group_name
+  ebs_01_volume_size = var.ebs_01_volume_size
+  ebs_02_volume_size = var.ebs_02_volume_size
+  engine = var.engine
+  engine_version = var.engine_version
+  identifier = var.identifier
+  instance_class = var.instance_class
+  instance_name = var.instance_name
   instance_type = var.instance_type
-
-  subnet_id       = local.ids_sorted_by_az[count.index % length(local.ids_sorted_by_az)]
-  vpc_security_group_ids = [var.vpc_security_group_ids]
-  root_block_device {
-    encrypted             = "true"
-    volume_type           = "gp3"
-    volume_size           = var.root_volume_size
-    delete_on_termination = "false"
-    kms_key_id            = var.kms_key_id
-  }
-
-  ebs_block_device {
-    encrypted             = "true"
-    device_name           = "/dev/sdb"
-    volume_type           = "gp3"
-    volume_size           = var.ebs_01_volume_size
-    delete_on_termination = "false"
-    kms_key_id            = var.kms_key_id
-  }
-
-  ebs_block_device {
-    encrypted             = "true"
-    device_name           = "/dev/sdc"
-    volume_type           = "gp3"
-    volume_size           = var.ebs_02_volume_size
-    delete_on_termination = "false"
-    kms_key_id            = var.kms_key_id
-  }
-
-  key_name  = var.key_name
-
-  disable_api_termination = "true"
-  disable_api_stop        = "true"
-
-  tags = merge({ "Name" = var.instance_name[count.index] }, var.tags)
-}
-
-##### RDS #####
-resource "aws_db_instance" "aws-terraform-rds" {
-    identifier                      = var.identifier
-    engine                          = var.engine
-    engine_version                  = var.engine_version
-    instance_class                  = var.instance_class
-
-    multi_az                        = var.multi_az
-    username                        = var.username
-    password                        = var.password
-    vpc_security_group_ids          = [var.vpc_security_group_ids]
-    db_subnet_group_name            = var.db_subnet_group_name
-    parameter_group_name            = var.parameter_group_name
-    option_group_name               = var.option_group_name
-    monitoring_interval             = 60
-    kms_key_id                      = var.kms_key_id
-    storage_encrypted               = true
-    storage_type                    = "gp3"
-    allocated_storage               = var.allocated_storage
-    deletion_protection             = true
-    backup_retention_period         = 35
-    monitoring_role_arn             = var.monitoring_role_arn
-    copy_tags_to_snapshot           = true
-    enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
-    auto_minor_version_upgrade      = false
-    skip_final_snapshot             = false
-
-    tags = merge({ "Name" = var.identifier }, var.tags)
+  key_name = var.key_name
+  kms_key_id = var.kms_key_id
+  monitoring_role_arn = var.monitoring_role_arn
+  multi_az = var.multi_az
+  option_group_name = var.option_group_name
+  parameter_group_name = var.parameter_group_name
+  password = var.password
+  region = var.region
+  role_arn = var.role_arn
+  root_volume_size = var.root_volume_size
+  session_name = var.session_name
+  subnet_id = var.subnet_id
+  tags = var.tags
+  username = var.username
+  vpc_id = var.vpc_id
+  vpc_security_group_ids = var.vpc_security_group_ids
 }
