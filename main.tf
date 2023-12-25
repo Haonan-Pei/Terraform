14aaf405e2ba3428964245870146cde490ee88d1
Haonan Pei <>
2023-12-25 10:46:20 +0800

    Update

--- main.tf
+++ main.tf
@@ -7,19 +7,28 @@ terraform {
   }
 }
 
-module "ec2instance" {
-  source  = "app.terraform.io/Haonan-Pei/ec2instance/aws"
-  version = "1.0.13"
+module "ec2_instance" {
+  source  = "app.terraform.io/eBaoTech/ec2-instance/aws"
+  version = "1.0.0"
+  # insert required variables here
 
-  region = var.region
-  instance_number = var.instance_number
-  vpc_security_group_ids = [var.vpc_security_group_ids]
-  role_arn     = var.role_arn
-  session_name = var.session_name
-  instance_type = var.instance_type
-  subnet_id       = var.subnet_id
-  root_volume_size = var.root_volume_size
-  ebs_volume_size  = var.ebs_volume_size
-  key_name  = var.key_name
-  tags = var.tags
+  region         = var.region
+  role_arn       = var.role_arn
+  session_name   = var.session_name
+  instance_name  = var.instance_name
+  instance_type  = var.instance_type
+  key_name       = var.key_name
+  ec2_kms_key_id = var.ec2_kms_key_id
+
+  #Network
+  vpc_id                     = var.vpc_id
+  subnet_id                  = var.subnet_id
+  ec2_vpc_security_group_ids = [var.ec2_vpc_security_group_ids]
+
+  #Storage
+  root_volume_size   = var.root_volume_size
+  ebs_01_volume_size = var.ebs_01_volume_size
+  ebs_02_volume_size = var.ebs_02_volume_size
+
+  ec2_tags = var.ec2_tags
 }
