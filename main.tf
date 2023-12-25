a31675e0ca8a144e72db0d30a65aa3c537190623
Haonan Pei <>
2023-12-25 11:17:00 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.30.0"
-    }
-  }
-}
-
 module "ec2_instance" {
   source  = "app.terraform.io/eBaoTech/ec2-instance/aws"
   version = "1.0.0"
