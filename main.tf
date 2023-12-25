c4a43aa54b283a547846f674b601b108a9eb5711
Haonan Pei <>
2023-12-25 12:00:50 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
+provider "aws" {
+  region = var.region
+  assume_role {
+    role_arn     = var.role_arn
+    session_name = var.session_name
+  }
+}
 module "ec2_instance" {
   source  = "app.terraform.io/eBaoTech/ec2-instance/aws"
   version = "1.0.0"
   # insert required variables here
 
-  region         = var.region
-  role_arn       = var.role_arn
-  session_name   = var.session_name
+  
   instance_name  = var.instance_name
   instance_type  = var.instance_type
   key_name       = var.key_name
