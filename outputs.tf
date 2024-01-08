67309562038196e2d40d44e592e7820b8f477cfc
Haonan Pei <130948725+Haonan-Pei@users.noreply.github.com>
2024-01-08 23:13:27 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,29 @@
-output "aws-sg-mis-tfc-test-node_instance_arn" {
-  description = "ARN of the EC2 instance"
-  value = module.aws-sg-mis-tfc-test-node.arn
+output "aws-sg-mis-tfc-test-node_instance_id" {
+  description = "ID of the EC2 instance"
+  value = module.aws-sg-mis-tfc-test-node[0].id
 }
 
-output "aws-sg-mis-tfc-test-node_instance_private_dns" {
+output "aws-sg-mis-tfc-test-node_instance_arn" {
+  description = "ARN of the EC2 instance"
+  value = module.aws-sg-mis-tfc-test-node[0].arn
+}
+
+output "aws-sg-mis-tfc-test-node_instance_private_ip" {
   description = "Private IP address of the EC2 instance"
-  value = module.aws-sg-mis-tfc-test-node.private_dns
+  value = module.aws-sg-mis-tfc-test-node[0].private_ip
+}
+
+output "aws-sg-mis-tfc-prod-node_instance_id" {
+  description = "ID of the EC2 instance"
+  value = module.aws-sg-mis-tfc-prod-node[0].id
 }
 
 output "aws-sg-mis-tfc-prod-node_instance_arn" {
   description = "ARN of the EC2 instance"
-  value = module.aws-sg-mis-tfc-prod-node.arn
+  value = module.aws-sg-mis-tfc-prod-node[0].arn
 }
 
-output "aws-sg-mis-tfc-prod-node_instance_private_dns" {
+output "aws-sg-mis-tfc-prod-node_instance_private_ip" {
   description = "Private IP address of the EC2 instance"
-  value = module.aws-sg-mis-tfc-prod-node.private_dns
+value = module.aws-sg-mis-tfc-prod-node[0].private_ip
 }
